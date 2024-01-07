decb65f3848985e941e85b2587f16293afc6c529
Carlos Gomez <carlosegs93@gmail.com>
2024-01-07 18:16:06 -0500

    Modelos creados

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    descripción: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    plataformas: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Imagen: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      isUrl: true
+    },
+    lanzamiento: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isDate: true
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+    {
+      timestamps: false
+    });
 };
