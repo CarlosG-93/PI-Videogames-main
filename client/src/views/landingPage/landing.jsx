0d92378fce333e474a86bfb5f14f47f99748bdc6
Carlos Gomez <carlosegs93@gmail.com>
2024-01-15 22:59:01 -0500

    Landing listo

--- client/src/views/landingPage/landing.jsx
+++ client/src/views/landingPage/landing.jsx
@@ -6,7 +6,6 @@ const Landing = () => {
     return (
         <div className={style.divOpen}>
             <div className={style.divButton}>
-                <img className={style.logo} src="../../../public/videogame.png" />
                 <Link className={style.link} to={'/home'}>
                     <button>ENTRAR</button>
                 </Link>
